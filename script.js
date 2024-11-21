28c7a8cf6bec7095873b49989cba9687092dc59c
Govind Ji <jigovind556@gmail.com>
2024-11-21 19:04:07 +0530

    modified the code to fetch excel from google drive

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let password = "";
 
+const pad = 1;
 document
   .getElementById("loginForm")
   .addEventListener("submit", function (event) {
@@ -9,7 +10,11 @@ document
 
     // Load the Excel file from the root directory
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "data.xlsx", true);
+    xhr.open(
+      "GET",
+      "https://docs.google.com/spreadsheets/d/1fMjDdGK9IIs7lm_jc0f-dj6V0MKHehZu8e3mx5EwhoY/edit?usp=sharing",
+      true
+    );
     xhr.responseType = "arraybuffer";
 
     xhr.onload = function (e) {
@@ -17,62 +22,62 @@ document
       var workbook = XLSX.read(data, { type: "array" });
       var sheetName = workbook.SheetNames[0];
       var sheet = workbook.Sheets[sheetName];
+      // console.log("data", sheet);
 
       var range = XLSX.utils.decode_range(sheet["!ref"]);
 
       var found = false;
-      let displayData = ""; // Variable to store display data
+      let displayData = "";
 
-      // Loop through each row to find the mobile number
       for (var rowNum = range.s.r + 1; rowNum <= range.e.r; rowNum++) {
         // Start from row 2 to skip header
-        var mobileCell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 1 })]; // Mobile Number in the second column
+        var mobileCell =
+          sheet[XLSX.utils.encode_cell({ r: rowNum, c: pad + 1 })];
+        // console.log("object", mobileCell);
         if (mobileCell && mobileCell.v == mobileNumber) {
-          // Fetch corresponding data from each column
           var studentNameCell =
-            sheet[XLSX.utils.encode_cell({ r: rowNum, c: 0 })]; // Student Name
+            sheet[XLSX.utils.encode_cell({ r: rowNum, c: pad + 0 })]; 
           var batchNameCell =
-            sheet[XLSX.utils.encode_cell({ r: rowNum, c: 2 })]; // Batch Name
-          var addressCell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 3 })]; // Full Delivery Address
+            sheet[XLSX.utils.encode_cell({ r: rowNum, c: pad + 2 })]; 
+          var addressCell =
+            sheet[XLSX.utils.encode_cell({ r: rowNum, c: pad + 3 })]; 
           var trackingIdCell =
-            sheet[XLSX.utils.encode_cell({ r: rowNum, c: 4 })]; // Tracking ID
-          var remarkCell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 5 })]; // Remark
+            sheet[XLSX.utils.encode_cell({ r: rowNum, c: pad + 4 })];
+          // var remarkCell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: pad + 5 })]; // Remark
 
-      // Construct the display content
-displayData = `
-    <table class="student-info-table">
-        <thead>
-            <tr>
-                <th>Particulars</th>
-                <th>Details</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Student Name</td>
-                <td>${studentNameCell.v}</td>
-            </tr>
-            <tr>
-                <td>Batch Name</td>
-                <td>${batchNameCell.v}</td>
-            </tr>
-            <tr>
-                <td>Full Delivery Address</td>
-                <td>${addressCell.v}</td>
-            </tr>
-            <tr>
-                <td>Tracking ID</td>
-                <td>${trackingIdCell.v}</td>
-            </tr>
-            
-            <tr>
-                <td>Track on Indian Postal Website</td>
-                <td><a href="https://www.indiapost.gov.in/_layouts/15/dop.portal.tracking/trackconsignment.aspx" target="_blank" rel="noopener noreferrer">Click here to track your consignment</a></td>
-            </tr>
-        </tbody>
-    </table>
-`;
-
+          displayData = `
+              <table class="student-info-table">
+                  <thead>
+                      <tr>
+                          <th>Particulars</th>
+                          <th>Details</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                          <td>Student Name</td>
+                          <td>${studentNameCell.v}</td>
+                      </tr>
+                      <tr>
+                          <td>Batch Name</td>
+                          <td>${batchNameCell.v}</td>
+                      </tr>
+                      <tr>
+                          <td>Full Delivery Address</td>
+                          <td>${addressCell.v}</td>
+                      </tr>
+                      <tr>
+                          <td>Tracking ID</td>
+                          <td>${trackingIdCell.v}</td>
+                      </tr>
+                      
+                      <tr>
+                          <td>Track on Indian Postal Website</td>
+                          <td><a href="https://www.indiapost.gov.in/_layouts/15/dop.portal.tracking/trackconsignment.aspx" target="_blank" rel="noopener noreferrer">Click here to track your consignment</a></td>
+                      </tr>
+                  </tbody>
+              </table>
+          `;
 
           found = true;
           break;
